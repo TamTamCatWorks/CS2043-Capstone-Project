db1132e095b03214b64a91b634c6ad3540b363ba
VU <vus20007@gmail.com>
2026-04-26 21:22:25 +0700

    feat: added item & art classes

---
+++ auction-system/auction/src/main/java/org/tamtamcatworks/auction/Art.java
@@ -0,0 +1,22 @@
+package org.tamtamcatworks.auction;
+
+/**
+ * Concrete class for art items 
+ */
+public class Art extends Item {
+    /*
+     * Each art item will have 
+     * artist(string)
+     * yearCreated(int)
+     * medium(string)
+     * 
+     */
+
+    /**
+     * Returns the detail of the object
+     */
+    @Override
+    public String getDisplayInfo() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ auction-system/auction/src/main/java/org/tamtamcatworks/auction/Item.java
@@ -0,0 +1,33 @@
+package org.tamtamcatworks.auction;
+
+/**
+ * Abstract Item class for all item listings
+ * 
+ * <p> extends from entity, remember to call super() in it
+ * 
+ */
+
+public abstract class Item extends Entity {
+    /*
+     * Each item will have:
+     * name(string)
+     * description(string)
+     * basePrice(double)
+     */
+
+    /**
+     * Constructor for the item class
+     * 
+     * <p> add relevant initilization parameters, remember to call super()
+     * 
+     */
+    protected Item() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    // Write setters, getters here
+
+    /**
+     * 
+     */
+}
