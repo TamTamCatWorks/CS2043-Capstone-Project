732a18736e4f70cf3ec1f2cf45bd5fcd7eaa7926
VU <vus20007@gmail.com>
2026-05-06 16:19:56 +0700

    some configurations for service

---
+++ auction-system/service/src/main/java/org/tamtamcatworks/auction/service/config/ServiceConfig.java
@@ -0,0 +1,14 @@
+package org.tamtamcatworks.auction.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class ServiceConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
