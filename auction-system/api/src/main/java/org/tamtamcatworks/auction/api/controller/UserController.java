f0eeca7a9926520697f09a063a0b4a7db7dee15a
VU <vus20007@gmail.com>
2026-05-06 23:29:40 +0700

    trimmed the dependencies for services

--- auction-system/api/src/main/java/org/tamtamcatworks/auction/api/controller/UserController.java
+++ auction-system/api/src/main/java/org/tamtamcatworks/auction/api/controller/UserController.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.SecurityContextRepository;
 
 import org.tamtamcatworks.auction.service.member.UserService;
@@ -56,12 +56,13 @@ public class UserController {
         HttpServletRequest request,
         HttpServletResponse response
     ) {
-        Authentication authentication = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(req.email(), req.password())
-        );
+        Authentication authenticationRequest =
+            UsernamePasswordAuthenticationToken.unauthenticated(req.email(), req.password());
+        Authentication authenticationResponse =
+            authenticationManager.authenticate(authenticationRequest);
 
         SecurityContext context = SecurityContextHolder.createEmptyContext();
-        context.setAuthentication(authentication);
+        context.setAuthentication(authenticationResponse);
         request.getSession(true);
         request.changeSessionId();
         securityContextRepository.saveContext(context, request, response);
