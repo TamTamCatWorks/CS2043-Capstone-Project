18de379a3344c3d4cb2c625b59099fae25614cd3
VU <vus20007@gmail.com>
2026-05-20 23:31:13 +0700

    fix: securities stuff

--- auction-system/api/src/main/java/org/tamtamcatworks/auction/api/config/WebSecurityConfig.java
+++ auction-system/api/src/main/java/org/tamtamcatworks/auction/api/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package org.tamtamcatworks.auction.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
@@ -59,6 +60,8 @@ public class WebSecurityConfig {
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/users/register", "/users/login").permitAll()
+                .requestMatchers(HttpMethod.GET, "/items/*").permitAll()
+                .requestMatchers(HttpMethod.GET, "/auctions", "/auctions/*", "/auctions/*/bids").permitAll()
                 .anyRequest().authenticated()
             );
         return http.build();
